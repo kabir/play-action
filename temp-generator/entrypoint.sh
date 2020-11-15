a12bf673e87a67d54f7e55b9a0b5d010a941db4c
Kabir Khan <kkhan@redhat.com>
2020-11-15 17:10:28 +0000

    Flesh out bash

--- temp-generator/entrypoint.sh
+++ temp-generator/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-###########################################################################################
+############################################################
 # Functions 
 
 checkCheckedOutRepo() {
@@ -18,10 +18,64 @@ checkCheckedOutRepo() {
   fi
 }
 
-###########################################################################################
-# Main code 
+############################################################
+# Main code
 
 cat ${GITHUB_EVENT_PATH}
 checkCheckedOutRepo
 
+# Get parameters from the event
+issue_body="$(jq '.issue.body' ${GITHUB_EVENT_PATH})"
+issue_number="$(jq '.issue.number' ${GITHUB_EVENT_PATH})"
+issue_title="$(jq '.issue.title' ${GITHUB_EVENT_PATH})"
+issue_url="$(jq '.issue.url' ${GITHUB_EVENT_PATH})"
+
+branch="multi-repo-ci-branch-${issue_number}"
+
+############################################################
+# Checkout
+git checkout -b "${branch}"
+
+############################################################
+# Get the issue body into config.yml
+touch config.yml
+echo "${issue_body}" > config.yml
+echo config.yml contents
+cat config.yml
+
+############################################################
+# Generate the workflow
+/multi-repo-ci-tool-runner generate-workflow --workflow-dir=.github/workflows --yaml=config.yml --issue=${issue_number} --branch=${branch}
+
+############################################################
+# Update the issue-data.json created by the tool to:
+# 1) Contain the user who updated the issue
+tmpfile=$(mktemp)
+jq --arg user "${GITHUB_ACTOR}" '.["trigger"]["user"]=$user' issue-data.json > "${tmpfile}"
+mv "${tmpfile}" issue-data.json
+# 2) Contain the url of the issue triggering the workflow
+tmpfile=$(mktemp)
+jq --arg url "${issue_url}" '.["trigger"]["issue"]=$url' issue-data.json > "${tmpfile}"
+mv "${tmpfile}" issue-data.json
+
+############################################################
+# Make sure .gitignore exists and that it contains .ci-tools.
+# The generated workflow clones the branch from this repository
+# into a sub-folder containing it into a sub-folder with this
+# name which in turn gives some warnings we don't ignore that
+# sub-folder
+if [[ ! -f .gitignore ]]; then
+  # Make sure .gitignore exists
+  touch .gitignore
+fi
+if ! grep -q .ci-tools .gitignore; then
+  # Add .ci-tools to .gitignore
+  echo .ci-tools >> .gitignore
+fi
+
+############################################################
+# Commit and push the workflow to the branch
+git add -A
+git commit -m "Generated Workflow: #${issue_number} - ${issue_title}"
+git push --force origin "${branch}"
 
