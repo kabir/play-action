d54db7887b2dd35bcf368315970ed8933d4ed898
Kabir Khan <kkhan@redhat.com>
2020-11-15 11:14:59 +0000

    Update entrypoint.sh

--- temp-generator/entrypoint.sh
+++ temp-generator/entrypoint.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -l
 
-# cat ${GITHUB_EVENT_PATH}
-checkCheckedOutRepo()
-
-
 # Functions
 
-function checkCheckedOutRepo() {
+checkCheckedOutRepo() {
+  echo Checking we have checked out ${GITHUB_ACTION_REPOSITORY}...
   if [[ ! -d ".git" ]]; then
-    echo Before using this action you need to use the checkout action as follows:
+    echo "Before using this action you need to use the checkout action as follows:"
     echo
     echo "- uses: actions/checkout@v2"
     echo "    with:"
     echo "      token: $\{\{ secrets.OB_MULTI_CI_PAT \}\}"
 
     exit 1
+  else
+    echo Have checked out ${GITHUB_ACTION_REPOSITORY}.
   fi
 }
 
-
+# cat ${GITHUB_EVENT_PATH}
+checkCheckedOutRepo
