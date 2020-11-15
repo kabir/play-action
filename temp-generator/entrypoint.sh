8e6035f35162fa5641fcf450b1c4538723526a48
Kabir Khan <kkhan@redhat.com>
2020-11-15 10:59:33 +0000

    Update entrypoint.sh

--- temp-generator/entrypoint.sh
+++ temp-generator/entrypoint.sh
@@ -1,16 +1,21 @@
 #!/bin/sh -l
 
 # cat ${GITHUB_EVENT_PATH}
+checkCheckedOutRepo()
 
-# For now assume the user cloned the repo using the checkout 
-if [[ ! -d ".git" ]]; then
-  echo Before using this action you need to use the checkout action as follows:
-  echo
-  echo - uses: actions/checkout@v2
-  echo     with:
-  echo       # The personal access token must be something which has the workflow permissions
-  echo       # (I also used repo, admin:repo_hook and user)
-  echo       token: ${{ secrets.OB_MULTI_CI_PAT }}
-  
-  exit 1
-fi
+
+# Functions
+
+function checkCheckedOutRepo() {
+  if [[ ! -d ".git" ]]; then
+    echo Before using this action you need to use the checkout action as follows:
+    echo
+    echo "- uses: actions/checkout@v2"
+    echo "    with:"
+    echo "      token: $\{\{ secrets.OB_MULTI_CI_PAT \}\}"
+
+    exit 1
+  fi
+}
+
+
