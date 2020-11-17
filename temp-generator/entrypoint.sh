2431afd3b4b1c2dcf47e4f2000171f6c79980e3c
Kabir Khan <kkhan@redhat.com>
2020-11-17 14:25:54 +0000

    Update entrypoint.sh

--- temp-generator/entrypoint.sh
+++ temp-generator/entrypoint.sh
@@ -3,7 +3,7 @@
 # keep track of the last executed command
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+trap 'echo "ERROR \"${last_command}\" failed with exit code $?."' EXIT
 
 
 ############################################################
